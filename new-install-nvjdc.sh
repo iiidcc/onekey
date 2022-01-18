94fcfb1c2ae53bfd8220cd06ce911bb21bd86b9a
Bulletgod <83668321+Bulletgod@users.noreply.github.com>
2022-01-18 16:02:45 +0800

    诺兰2.4

--- new-install-nvjdc.sh
+++ new-install-nvjdc.sh
@@ -155,17 +155,17 @@ exit
 
 install_nvjdc(){
 echo -e "${red}开始进行安装,请根据命令提示操作${plain}"
-git clone https://ghproxy.com/https://github.com/NolanHzy/nvjdcdocker.git /root/nvjdc
-if [ ! -d "/root/nvjdc/.local-chromium/Linux-884014" ]; then
-cd nvjdc
+git clone git clone https://ghproxy.com/https://github.com/Bulletgod/nvjdcdocker.git /root/nolanjdc
+if [ ! -d "/root/nolanjdc/.local-chromium/Linux-884014" ]; then
+cd nolanjdc
 echo -e "${green}正在拉取chromium-browser-snapshots等依赖,体积100多M，请耐心等待下一步命令提示···${plain}"
 mkdir -p  .local-chromium/Linux-884014 && cd .local-chromium/Linux-884014
 wget http://npm.taobao.org/mirrors/chromium-browser-snapshots/Linux_x64/884014/chrome-linux.zip > /dev/null 2>&1 
 unzip chrome-linux.zip > /dev/null 2>&1 
 rm  -f chrome-linux.zip > /dev/null 2>&1 
 fi
-mkdir /root/nvjdc/Config && cd /root/nvjdc/Config
-wget -O Config.json   https://ghproxy.com/https://raw.githubusercontent.com/NolanHzy/nvjdc/main/Config.json
+mkdir /root/nolanjdc/Config && cd /root/nolanjdc/Config
+wget -O Config.json   https://raw.githubusercontent.com/Bulletgod/nvjdcdocker/main/Config/Config.json
 read -p "请输入青龙服务器在web页面中显示的名称: " QLName && printf "\n"
 read -p "请输入nvjdc面板标题: " title && printf "\n"
 read -p "请输入nvjdc面板希望使用的端口号: " portinfo && printf "\n"
@@ -178,112 +178,132 @@ read -p "请输入青龙OpenApi Client Secret: " ClientSecret && printf "\n"
 read -p "请输入青龙服务器的url地址（类似http://192.168.2.2:5700）: " QLurl && printf "\n"
 read -p "请输入WP_APP_TOKEN: " WxPushertoken && printf "\n"
 read -p "请输入WxMainWP_UID: " WxMainWP_UID && printf "\n"
-cat > /root/nvjdc/Config/Config.json << EOF
+cat > /root/nolanjdc/Config/Config.json << EOF
 {
   ///浏览器最多几个网页
-  "MaxTab": "8",
+  "MaxTab": "4",
+  //网站标题
+  "Title": "我是标题",
   //回收时间分钟 不填默认3分钟
   "Closetime": "5",
-  //网站标题
-  "Title": "${title}",
   //网站公告
-  "Announcement": "本项目脚本收集于互联网。为了您的财产安全，请关闭京东免密支付。",
-  ///XDD PLUS Url  http://IP地址:端口/api/login/smslogin
-  "XDDurl": "${XDDurl}",
-  ///xddToken
-  "XDDToken": "${XDDToken}",
-  ///自动滑块次数5次 5次后手动滑块 可设置为0默认手动滑块
-
-  ///开启打印等待日志卡短信验证登陆 可开启 拿到日志群里回复
+  "Announcement": "为提高账户的安全性，请关闭免密支付。",
+   //Proxy 支持不带密码的socks5 以及http 
+  ///http  Proxy 只需要填写 ip:端口
+  /// Socks5 需要填写socks5://ip:端口 不能填写下方账户密码
+  "Proxy": "",
+  //Proxy帐号
+  "ProxyUser": "",
+  //Proxy密码
+  "ProxyPass": "",
+  //Opencv镜像地址  刚刚镜像的地址  ARM多一个配置 Captchaurl
+  "Captchaurl": "http://xxxxx:5703",
+  ///开启打印等待日志卡短信验证登陆 可开启 拿到日志群里回复 默认不要填写
   "Debug": "",
+  ///自动滑块次数5次 5次后手动滑块 可设置为0默认手动滑块
+  "AutoCaptchaCount": "5",
+  ///XDD PLUS Url  http://IP地址:端口/api/login/smslogin
+  "XDDurl": "",
+  ///xddToken
+  "XDDToken": "",
   ///登陆预警 0 0 12 * * ?  每天中午十二点 https://www.bejson.com/othertools/cron/ 表达式在线生成网址
-    "ExpirationCron": " 0 0 12 * * ?",
+  "ExpirationCron": " 0 0 12 * * ?",
   ///个人资产 0 0 10,20 * * ?  早十点晚上八点
-    "BeanCron": "0 0 10,20 * * ?",
+  "BeanCron": "0 0 10,20 * * ?",
   // ======================================= WxPusher 通知设置区域 ===========================================
   // 此处填你申请的 appToken. 官方文档：https://wxpusher.zjiecode.com/docs
   // WP_APP_TOKEN 可在管理台查看: https://wxpusher.zjiecode.com/admin/main/app/appToken
   // MainWP_UID 填你自己uid
   ///这里的通知只用于用户登陆 删除 是给你的通知
-  "WP_APP_TOKEN": "${WxPushertoken}",
-  "MainWP_UID": "${WxMainWP_UID}",
+  "WP_APP_TOKEN": "",
+  "MainWP_UID": "",
   // ======================================= pushplus 通知设置区域 ===========================================
-  ///Push Plus官方网站：http" //www.pushplus.plus  只有青龙模式有用
+  ///Push Plus官方网站：http: //www.pushplus.plus  只有青龙模式有用
   ///下方填写您的Token，微信扫码登录后一对一推送或一对多推送下面的token，只填" "PUSH_PLUS_TOKEN",
   "PUSH_PLUS_TOKEN": "",
   //下方填写您的一对多推送的 "群组编码" ，（一对多推送下面->您的群组(如无则新建)->群组编码）
   "PUSH_PLUS_USER": "",
-
-  ///青龙配置 注意 如果不要青龙  Config :[]
+  ///青龙配置  注意对接XDD 对接芝士 设置为"Config":[]
   "Config": [
     {
       //序号必填从1 开始
       "QLkey": 1,
       //服务器名称
-      "QLName": "${QLName}",
+      "QLName": "阿里云",
       //青龙地址
-      "QLurl": "${QLurl}",
+      "QLurl": "http://ip:5700",
       //青龙2,9 OpenApi Client ID
-      "QL_CLIENTID": "${ClientID}",
+      "QL_CLIENTID": "",
       //青龙2,9 OpenApi Client Secret
-      "QL_SECRET": "${ClientSecret}",
+      "QL_SECRET": "",
       //CK最大数量
-      "QL_CAPACITY": 40,
+      "QL_CAPACITY": 99,
       ///建议一个青龙一个WxPusher 应用
-      "WP_APP_TOKEN": "${WxPushertoken}"
+      "WP_APP_TOKEN": ""
     }
   ]
 
 }
+
 EOF
 else
-cat > /root/nvjdc/Config/Config.json << EOF
+cat > /root/nolanjdc/Config/Config.json << EOF
 {
-    ///浏览器最多几个网页
-    "MaxTab": "8",
-    //回收时间分钟 不填默认3分钟
-    "Closetime": "5",
-    //网站标题
-    "Title": "${title}",
-    //网站公告
-    "Announcement": "本项目脚本收集于互联网。为了您的财产安全，请关闭京东免密支付。",
-    ///XDD PLUS Url  http://IP地址:端口/api/login/smslogin
-    "XDDurl": "${XDDurl}",
-    ///xddToken
-    "XDDToken": "${XDDToken}",
-    ///自动滑块次数5次 5次后手动滑块 可设置为0默认手动滑块
-
-    ///开启打印等待日志卡短信验证登陆 可开启 拿到日志群里回复
-    "Debug": "",
-    ///登陆预警 0 0 12 * * ?  每天中午十二点 https://www.bejson.com/othertools/cron/ 表达式在线生成网址
-      "ExpirationCron": " 0 0 12 * * ?",
-    ///个人资产 0 0 10,20 * * ?  早十点晚上八点
-      "BeanCron": "0 0 10,20 * * ?",
-    // ======================================= WxPusher 通知设置区域 ===========================================
-    // 此处填你申请的 appToken. 官方文档：https://wxpusher.zjiecode.com/docs
-    // WP_APP_TOKEN 可在管理台查看: https://wxpusher.zjiecode.com/admin/main/app/appToken
-    // MainWP_UID 填你自己uid
-    ///这里的通知只用于用户登陆 删除 是给你的通知
-  "WP_APP_TOKEN": "${WxPushertoken}",
-  "MainWP_UID": "${WxMainWP_UID}",
-    // ======================================= pushplus 通知设置区域 ===========================================
-    ///Push Plus官方网站：http" //www.pushplus.plus  只有青龙模式有用
-    ///下方填写您的Token，微信扫码登录后一对一推送或一对多推送下面的token，只填" "PUSH_PLUS_TOKEN",
-    "PUSH_PLUS_TOKEN": "",
-    //下方填写您的一对多推送的 "群组编码" ，（一对多推送下面->您的群组(如无则新建)->群组编码）
-    "PUSH_PLUS_USER": "",
-
-  ///青龙配置 注意 如果不要青龙  Config :[]
+  ///浏览器最多几个网页
+  "MaxTab": "4",
+  //网站标题
+  "Title": "我是标题",
+  //回收时间分钟 不填默认3分钟
+  "Closetime": "5",
+  //网站公告
+  "Announcement": "为提高账户的安全性，请关闭免密支付。",
+   //Proxy 支持不带密码的socks5 以及http 
+  ///http  Proxy 只需要填写 ip:端口
+  /// Socks5 需要填写socks5://ip:端口 不能填写下方账户密码
+  "Proxy": "",
+  //Proxy帐号
+  "ProxyUser": "",
+  //Proxy密码
+  "ProxyPass": "",
+  //Opencv镜像地址  刚刚镜像的地址  ARM多一个配置 Captchaurl
+  "Captchaurl": "http://xxxxx:5703",
+  ///开启打印等待日志卡短信验证登陆 可开启 拿到日志群里回复 默认不要填写
+  "Debug": "",
+  ///自动滑块次数5次 5次后手动滑块 可设置为0默认手动滑块
+  "AutoCaptchaCount": "5",
+  ///XDD PLUS Url  http://IP地址:端口/api/login/smslogin
+  "XDDurl": "",
+  ///xddToken
+  "XDDToken": "",
+  ///登陆预警 0 0 12 * * ?  每天中午十二点 https://www.bejson.com/othertools/cron/ 表达式在线生成网址
+  "ExpirationCron": " 0 0 12 * * ?",
+  ///个人资产 0 0 10,20 * * ?  早十点晚上八点
+  "BeanCron": "0 0 10,20 * * ?",
+  // ======================================= WxPusher 通知设置区域 ===========================================
+  // 此处填你申请的 appToken. 官方文档：https://wxpusher.zjiecode.com/docs
+  // WP_APP_TOKEN 可在管理台查看: https://wxpusher.zjiecode.com/admin/main/app/appToken
+  // MainWP_UID 填你自己uid
+  ///这里的通知只用于用户登陆 删除 是给你的通知
+  "WP_APP_TOKEN": "",
+  "MainWP_UID": "",
+  // ======================================= pushplus 通知设置区域 ===========================================
+  ///Push Plus官方网站：http: //www.pushplus.plus  只有青龙模式有用
+  ///下方填写您的Token，微信扫码登录后一对一推送或一对多推送下面的token，只填" "PUSH_PLUS_TOKEN",
+  "PUSH_PLUS_TOKEN": "",
+  //下方填写您的一对多推送的 "群组编码" ，（一对多推送下面->您的群组(如无则新建)->群组编码）
+  "PUSH_PLUS_USER": "",
+  ///青龙配置  注意对接XDD 对接芝士 设置为"Config":[]
   "Config": []
 
 }
+
 EOF
 fi
 read -p "请输入自动滑块次数 直接回车默认5次后手动滑块 输入0为默认手动滑块: " AutoCaptcha && printf "\n"
 	if [ ! -n "$AutoCaptcha" ];then
-    sed -i "14a \        \"AutoCaptchaCount\": \"5\"," /root/nvjdc/Config/Config.json
+    sed -i "14a \        \"AutoCaptchaCount\": \"5\"," /root/nolanjdc/Config/Config.json
 else
-    sed -i "14a \        \"AutoCaptchaCount\": \"${AutoCaptcha}\"," /root/nvjdc/Config/Config.json
+    sed -i "14a \        \"AutoCaptchaCount\": \"${AutoCaptcha}\"," /root/nolanjdc/Config/Config.json
 fi
 read -p "请输入要安装的nvjdc版本，如安装最新版直接回车: " version && printf "\n"
 	if [ ! -n "${version}" ];then
@@ -302,15 +322,15 @@ fi
 
 #拉取nvjdc镜像
 echo -e  "${green}开始拉取nvjdc镜像文件，nvjdc镜像比较大，请耐心等待${plain}"
-docker pull nolanhzy/nvjdc:${version1}
+docker pull Bulletplus/nvjdc:${version1}
 
 
 #创建并启动nvjdc容器
-cd /root/nvjdc
+cd /root/nolanjdc
 echo -e "${green}开始创建nvjdc容器${plain}"
 docker run   --name nvjdc -p ${portinfo}:80 -d  -v  "$(pwd)":/app \
 -v /etc/localtime:/etc/localtime:ro \
--it --privileged=true  nolanhzy/nvjdc:${version1}
+-it --privileged=true  bulletplus/nvjdc:${version1}
 docker update --restart=always nvjdc
 
 baseip=$(curl -s ipip.ooo)  > /dev/null
@@ -320,10 +340,10 @@ echo -e "${green}安装完毕,面板访问地址：http://${baseip}:${portinfo}$
 
 update_nvjdc(){
 mv /root/nvjdc /root/nvjdcdb
-git clone https://ghproxy.com/https://github.com/NolanHzy/nvjdcdocker.git /root/nvjdc
-cd /root/nvjdc &&  mkdir -p  Config &&  mv /root/nvjdcdb/Config.json /root/nvjdc/Config/Config.json
-cd /root/nvjdc &&    mv /root/nvjdcdb/.local-chromium /root/nvjdc/.local-chromium
-cd /root/nvjdc
+git clone https://ghproxy.com/https://github.com/bulletgod/nvjdcdocker.git /root/nolanjdc
+cd /root/nolanjdc &&  mkdir -p  Config &&  mv /root/nvjdcdb/Config.json /root/nolanjdc/Config/Config.json
+cd /root/nolanjdc &&    mv /root/nvjdcdb/.local-chromium /root/nolanjdc/.local-chromium
+cd /root/nolanjdc
 portinfo=$(docker port nvjdc | head -1  | sed 's/ //g' | sed 's/80\/tcp->0.0.0.0://g')
 condition=$(cat /root/nvjdc/Config/Config.json | grep -o '"XDDurl": .*' | awk -F":" '{print $1}' | sed 's/\"//g')
 AutoCaptcha1=$(cat /root/nvjdc/Config/Config.json | grep -o '"AutoCaptchaCount": .*' | awk -F":" '{print $1}' | sed 's/\"//g')
@@ -340,17 +360,17 @@ fi
 if [ ! -n "$AutoCaptcha1" ];then
 	read -p "请输入自动滑块次数 直接回车默认5次后手动滑块 输入0为默认手动滑块: " AutoCaptcha && printf "\n"
 	if [ ! -n "$AutoCaptcha" ];then
-    sed -i "5a \        \"AutoCaptchaCount\": \"5\"," /root/nvjdc/Config/Config.json
+    sed -i "5a \        \"AutoCaptchaCount\": \"5\"," /root/nolanjdc/Config/Config.json
 else
-    sed -i "5a \        \"AutoCaptchaCount\": \"${AutoCaptcha}\"," /root/nvjdc/Config/Config.json
+    sed -i "5a \        \"AutoCaptchaCount\": \"${AutoCaptcha}\"," /root/nolanjdc/Config/Config.json
 fi
 fi
 baseip=$(curl -s ipip.ooo)  > /dev/null
 docker rm -f nvjdc
-docker pull nolanhzy/nvjdc:latest
+docker pull Bulletplus/nvjdc
 docker run   --name nvjdc -p ${portinfo}:80 -d  -v  "$(pwd)":/app \
 -v /etc/localtime:/etc/localtime:ro \
--it --privileged=true  nolanhzy/nvjdc:latest
+-it --privileged=true  Bulletplus/nvjdc
 docker update --restart=always nvjdc
 echo -e "${green}nvjdc更新完毕，脚本自动退出。${plain}"
 exit 0
@@ -358,7 +378,7 @@ exit 0
 
 uninstall_nvjdc(){
 docker rm -f nvjdc
-rm -rf /root/nvjdc
+rm -rf /root/nolanjdc
 echo -e "${green}nvjdc面板已卸载，脚本自动退出，请手动删除nvjdc的镜像。${plain}"
 exit 0
 }
@@ -366,8 +386,8 @@ exit 0
 menu() {
   echo -e "\
 ${green}0.${plain} 退出脚本
-${green}1.${plain} 安装nvjdc2.2
-${green}2.${plain} 升级nvjdc2.2
+${green}1.${plain} 安装nvjdc2.4
+${green}2.${plain} 升级nvjdc2.4
 ${green}3.${plain} 卸载nvjdc
 "
 get_system_info
