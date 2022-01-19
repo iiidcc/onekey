0e66b9e35dfefdc816305b5d079d674a43f846a3
Bulletgod <83668321+Bulletgod@users.noreply.github.com>
2022-01-19 10:20:16 +0800

    1

--- new-install-nvjdc.sh
+++ new-install-nvjdc.sh
@@ -144,8 +144,11 @@ copyright(){
     clear
 echo -e "
 —————————————————————————————————————————————————————————————
-        Nvjdc自助面板一键安装脚本                         
- ${green}             
+        Nvjdc自助面板一键安装脚本                   
+ ${green}          
+                               ----v2.4 
+                             
+        小白一路回车，拒绝花里胡哨        
 —————————————————————————————————————————————————————————————
 "
 }
@@ -155,6 +158,7 @@ exit
 
 install_nvjdc(){
 echo -e "${red}开始进行安装,请根据命令提示操作${plain}"
+apt install git -y || yum install git -y > /dev/null 
 git clone https://ghproxy.com/https://github.com/Bulletgod/nvjdcdocker.git /root/nolanjdc && cd nolanjdc
 echo -e "${green}正在拉取chromium-browser-snapshots等依赖,体积100多M，请耐心等待下一步命令提示···${plain}"
 mkdir -p  .local-chromium/Linux-884014 && cd .local-chromium/Linux-884014
