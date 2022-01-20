c51b5f84b132c79e34f3d026422d76f5d1975c09
Bulletgod <83668321+Bulletgod@users.noreply.github.com>
2022-01-20 12:06:50 +0800

    1.0

--- new-install-Ark1.0.sh
+++ new-install-Ark1.0.sh
@@ -161,7 +161,7 @@ echo -e "${red}开始进行安装,请根据命令提示操作${plain}"
 apt install git -y || yum install git -y > /dev/null
 git clone https://ghproxy.com/https://github.com/NNNNolan/Ark.git /root/Ark
 if [ ! -d "/root/Ark/.local-chromium/Linux-884014" ]; then
-cd Ark
+cd /root/Ark
 echo -e "${green}正在拉取chromium-browser-snapshots等依赖,体积100多M，请耐心等待下一步命令提示···${plain}"
 mkdir -p  .local-chromium/Linux-884014 && cd .local-chromium/Linux-884014
 wget https://mirrors.huaweicloud.com/chromium-browser-snapshots/Linux_x64/884014/chrome-linux.zip > /dev/null 2>&1 
